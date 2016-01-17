c63dc0ac6cf868d1b3a5490b4ed5f8b5a217ecb6
heqs <hequanshui123@qq.com>
2016-01-17 20:03:44 +0800

    2016.1.17

--- app/src/main/java/com/spshop/MainActivity.java
+++ app/src/main/java/com/spshop/MainActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -16,10 +17,22 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.spshop.AdRouter.AdRouterListener;
 import com.nostra13.universalimageloader.core.ImageLoader;
+
+import org.json.JSONObject;
 
 
 public class MainActivity extends Activity {
@@ -41,6 +54,33 @@ public class MainActivity extends Activity {
         mImageUrl.add(imageUrl2);
         mAdView = (AdRouter) findViewById(R.id.ad_view);
         mAdView.setImageResources(mImageUrl, mAdCycleViewListener);
+
+        RequestQueue mQueue = Volley.newRequestQueue(MainActivity.this);
+        StringRequest stringRequest = new StringRequest(Request.Method.POST, "http://spshop.dbdy.net/api/app.asmx/GetAdvsByCname", new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                Log.d("TAG", response);
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("TAG", error.getMessage(), error);
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {  //设置头信息
+                Map<String, String> map = new HashMap<String, String>();
+                map.put("Content-Type", "application/x-www-form-urldecoded");
+                return map;
+            }
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> map = new HashMap<String, String>();
+                map.put("cname", "index_a1");
+                map.put("num", "4");
+                return map;
+            }
+        };
+        mQueue.add(stringRequest);
     }
 
     private AdRouterListener mAdCycleViewListener = new AdRouterListener() {
@@ -48,7 +88,7 @@ public class MainActivity extends Activity {
         @Override
         public void onImageClick(int position, View imageView) {
             // TODO 单击图片处理事件
-            Toast.makeText(MainActivity.this, "position->" + position, 0).show();
+            //Toast.makeText(MainActivity.this, "position->" + position, 0).show();
         }
 
         @Override
