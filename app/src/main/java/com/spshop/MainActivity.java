21b6e83ed6c7e283f1da192dde47795bb8f285eb
heqs <hequanshui123@qq.com>
2016-01-18 17:34:52 +0800

    2016.1.18

--- app/src/main/java/com/spshop/MainActivity.java
+++ app/src/main/java/com/spshop/MainActivity.java
@@ -25,13 +25,16 @@ import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageLoader;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
-import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+//import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.spshop.AdRouter.AdRouterListener;
-import com.nostra13.universalimageloader.core.ImageLoader;
+//import com.nostra13.universalimageloader.core.ImageLoader;
 
+import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 
@@ -39,48 +42,51 @@ public class MainActivity extends Activity {
 
 
     private AdRouter mAdView;
-
     private ArrayList<String> mImageUrl = null;
-
-    private String imageUrl1 = "http://shop.qz12t.cn/template/s01/images/0827f1.jpg";
-    private String imageUrl2 = "http://shop.qz12t.cn/template/s01/images/index_a3.jpg";
+    private RequestQueue mQueue = Volley.newRequestQueue(MainActivity.this);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mImageUrl = new ArrayList<String>();
-        mImageUrl.add(imageUrl1);
-        mImageUrl.add(imageUrl2);
-        mAdView = (AdRouter) findViewById(R.id.ad_view);
-        mAdView.setImageResources(mImageUrl, mAdCycleViewListener);
 
-        RequestQueue mQueue = Volley.newRequestQueue(MainActivity.this);
-        StringRequest stringRequest = new StringRequest(Request.Method.POST, "http://spshop.dbdy.net/api/app.asmx/GetAdvsByCname", new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                Log.d("TAG", response);
-            }
-        }, new Response.ErrorListener() {
+        //获取广告图片
+        Map<String, String> map = new HashMap<String, String>();
+        map.put("cname", "index_a1");
+        map.put("num", "4");
+        JSONObject params = new JSONObject(map);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, "http://spshop.dbdy.net/api/app.asmx/GetAdvsByCname", params,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("TAG", response.toString());
+                        String adJson = response.toString();
+                        try {
+                            JSONObject jsonObject = new JSONObject(adJson);
+                            JSONArray jsonArray = jsonObject.getJSONArray("d");
+                            //设置广告图片
+                            mImageUrl = new ArrayList<String>();
+
+                            for (int i = 0; i < jsonArray.length(); i++) {
+                                JSONObject jsonObject2 = (JSONObject) jsonArray.get(i);
+                                mImageUrl.add(jsonObject2.getString("ImgUrl"));
+                            }
+                            mAdView = (AdRouter) findViewById(R.id.ad_view);
+                            mAdView.setImageResources(mImageUrl, mAdCycleViewListener);
+
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
             @Override
             public void onErrorResponse(VolleyError error) {
                 Log.e("TAG", error.getMessage(), error);
             }
-        }) {
-            @Override
-            public Map<String, String> getHeaders() throws AuthFailureError {  //设置头信息
-                Map<String, String> map = new HashMap<String, String>();
-                map.put("Content-Type", "application/x-www-form-urldecoded");
-                return map;
-            }
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String, String> map = new HashMap<String, String>();
-                map.put("cname", "index_a1");
-                map.put("num", "4");
-                return map;
-            }
-        };
-        mQueue.add(stringRequest);
+        });
+        mQueue.add(jsonObjectRequest);
+
+
     }
 
     private AdRouterListener mAdCycleViewListener = new AdRouterListener() {
@@ -93,9 +99,10 @@ public class MainActivity extends Activity {
 
         @Override
         public void displayImage(String imageURL, ImageView imageView) {
-            ImageLoader imageLoader = ImageLoader.getInstance();
-            imageLoader.init(ImageLoaderConfiguration.createDefault(MainActivity.this));
-            imageLoader.displayImage(imageURL, imageView);
+            //ImageLoader imageLoader = new ImageLoader(mQueue,new ImageLoader.ImageCache());
+            //ImageLoader imageLoader = ImageLoader.getInstance();
+            //imageLoader.init(ImageLoaderConfiguration.createDefault(MainActivity.this));
+            //imageLoader.displayImage(imageURL, imageView);
         }
     };
 
